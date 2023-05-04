efb63db859a3879a48c187db9a73fa616c90b1e2
Saaoz <bastie_nb1998@hotmail.fr>
2023-05-04 16:43:02 +0200

    page create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/index';
 import Single from './pages/Single/index';
 import ChantierExistant from './pages/Chantier_Existant/index';
+import Create from './pages/Create_project/index'
 
 import './App.css';
 
@@ -12,7 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/chantier/:id" element={<Single />} />
                 <Route path="/chantier_existant/" element={<ChantierExistant />} />
-                <Route path="/create" element={<Single />} />
+                <Route path="/create" element={<Create />} />
             </Routes>
     </div>
   );
