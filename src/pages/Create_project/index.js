f475f9e41e7b42dc9a3bfd379fc150acb6c2064d
Saaoz <bastie_nb1998@hotmail.fr>
2023-05-04 17:04:48 +0200

    .

---
+++ src/pages/Create_project/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+
+
+function Create_project() {
+    return (
+        <>
+            <p>MA PAGE UWU GAMING</p>
+        </>
+    );
+  }
+  export default Create_project;
