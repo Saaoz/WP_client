5f70a39d92262232d553ea15853b5f04a7c5c225
dj-wib <wibdev7@gmail.com>
2023-05-04 15:59:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/chantier/:id" element={<Single />} />
-
             </Routes>
     </div>
   );
