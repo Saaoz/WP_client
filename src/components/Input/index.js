c78848295f157a7c07fbdfcb3707bbdf535fc224
dj-wib <wibdev7@gmail.com>
2023-05-05 16:39:02 +0200

    css input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,14 +3,16 @@ import './index.css';
 const Input = ({placeholder, type, id, onChange, value} ) => {
 
         return (
-            <input 
-                id={id}
-                className='input'
-                type={type}
-                placeholder={placeholder}
-                onChange={onChange}
-                value={value}
+        <div className='input-container'>
+                <input 
+                        id={id}
+                        className='input'
+                        type={type}
+                        placeholder={placeholder}
+                        onChange={onChange}
+                        value={value}
                 />
+        </div>
         );
 };
 
