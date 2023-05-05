5574f00a6ddba5780c2cf1049dd4ba776bf77014
Dimebagou <bersonvictor@gmail.com>
2023-05-05 11:32:12 +0200

    starting worksite create form functions

--- src/pages/Create_project/index.js
+++ src/pages/Create_project/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import './index.css';
@@ -5,22 +6,26 @@ import '../../components/Button/index.css'
 import '../../components/Input/index.css'
 
 function Create_project() {
+    const [worksite_name, setWorksite_name] = useState('');
+    const [street, setStreet] = useState('');
+    const [postal_code, setPostal_code] = useState('');
+    const [city, setCity] = useState('');
 
-    const handleSubmit = () => {
-        alert('fonction pour envoyer les données en base de données')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(worksite_name, street, postal_code, city);
     }
-
-
+    
     return (
         <container className='back_formu'>
             <form className='formulaire' >
                 <h1>Creation d’un nouveau project </h1>
                 <div className='input_zone'>
-                    <Input className="input input_c" type="text" placeholder='Nom du chantier' />
-                    <Input className="input input_a" type="text" placeholder='Adresse' />
+                    <Input className="input input_c" value={worksite_name} type="text" placeholder='Nom du chantier' onChange={(e) => setWorksite_name(e.target.value) } required/>
+                    <Input className="input input_a" value={street} type="text" placeholder='Numéro et nom de la rue' onChange={(e) => setStreet(e.target.value)} required/>
                     <div className='input_bot'>
-                        <Input className="input input_p" type="text" placeholder='Code postal' />
-                        <Input className="input input_v" type="text" placeholder=' Ville' />
+                        <Input className="input input_p" value={postal_code} type="text" placeholder='Code postal' onChange={(e) => setPostal_code(e.target.value) } required/>
+                        <Input className="input input_v" value={city} type="text" placeholder=' Ville' onChange={(e) => setCity(e.target.value)} required/>
                     </div>
                 </div>
                 <div className='btn_zone'>
@@ -30,5 +35,5 @@ function Create_project() {
             </form>
         </container>
     );
-  }
-  export default Create_project;
+}
+export default Create_project;
