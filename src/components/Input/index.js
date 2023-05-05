41fa9ee3f39a5a6b40ad9ba889c5e9772db917ca
dj-wib <wibdev7@gmail.com>
2023-05-05 15:36:34 +0200

    update input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,50 +1,17 @@
 import './index.css';
-import search from '../../sources/imgs/search.svg';
-import localisation from '../../sources/imgs/localisationIcon.svg';
 
-const Input = ({placeholder, type, id, onChange, key, value} ) => {
+const Input = ({placeholder, type, id, onChange, value} ) => {
 
-    let srcIcone;
-
-switch (type) {
-    case 'search':
-        srcIcone = search;
-        break;
-    case 'localisation':
-        srcIcone = localisation;
-        break;
-    default:
-        break;
-}
-
-    if(srcIcone){
         return (
-            <div className='input-container'>
             <input 
                 id={id}
                 className='input'
-                type="text"
-                placeholder={placeholder}
-                value={value}
-                />
-            <img className='icon-input' src={srcIcone} alt='logo type of input'></img>
-            </div>
-        );
-    }else{
-
-
-        return (
-            <div className='input-container'>
-            <input 
-                id={id}
-                className='input'
-                type="text"
+                type={type}
                 placeholder={placeholder}
                 onChange={onChange}
                 value={value}
                 />
-            </div>
-        );}
+        );
 };
 
 export default Input;
--- src/pages/Create_worksmanagers/index.js
+++ src/pages/Create_worksmanagers/index.js
@@ -1,4 +1,5 @@
 import './index.css';
+import Header from '../../components/Header';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
@@ -9,17 +10,20 @@ const Create_worksmanagers = () => {
 
     return (
         <div className='create-worksmanagers-page'>
+            <Header/>
             <div className='create-worksmanagers-container'>
                 <h1>Create new workmanager</h1>
                 <form className='create-worksmanagers-form'>
-                <Input />
-                <Input />
-                <Input />
-                <Input />
-                <Input />
+                    <Input placeholder='First name' type='text' id='firstName'/>
+                    <Input placeholder='Last name' type='text' id='lastName'/>
+                    <Input placeholder='Email' type='email' id='email'/>
 
-                <Input className='create-worksmanagers-button' type='submit' value="Validate"/>
-                <Button className='btn1' value='Cancel' onClick={()=>navigate('/')}/>
+                    <Input placeholder='Login' type='text' id='login'/>
+                    <Input placeholder='Password' type='password' id='password'/>
+                    <div className='cta-container'>
+                        <input className='btn1' type='submit' value='Create'/>
+                        <Button className='btn1' value='Cancel' onClick={()=>navigate('/')}/>
+                    </div>
                 </form>
             </div>
         </div>
