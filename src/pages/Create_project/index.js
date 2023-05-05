4dfc9ea1d5e07a94aaacd4908c1874451f47a241
Saaoz <bastie_nb1998@hotmail.fr>
2023-05-05 10:57:22 +0200

    [UPDATE] mise en place formulaire création chantier

--- src/pages/Create_project/index.js
+++ src/pages/Create_project/index.js
@@ -1,19 +1,28 @@
 import Button from '../../components/Boutton';
 import Input from '../../components/Input';
 import './index.css';
-
-
+import '../../components/Boutton/index.css'
+import '../../components/Input/index.css'
 
 function Create_project() {
     return (
-        <>
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Button/>
-            <Button/>
-        </>
+        <container className='back_formu'>
+            <container className='formulaire' >
+                <h1>Creation d’un nouveau project </h1>
+                <div className='input_zone'>
+                    <Input className="input input_c" type="text" placeholder='Nom du chantier' />
+                    <Input className="input input_a" type="text" placeholder='Adresse' />
+                    <div className='input_bot'>
+                        <Input className="input input_p" type="text" placeholder='Code postal' />
+                        <Input className="input input_v" type="text" placeholder=' Ville' />
+                    </div>
+                </div>
+                <div className='btn_zone'>
+                    <Button className="btn2" value="Valider" />
+                    <Button className="btn2" value="Retour" />
+                </div>
+            </container>
+        </container>
     );
   }
   export default Create_project;
